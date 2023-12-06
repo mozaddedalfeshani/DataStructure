0d0d0e08bc4467deeae3636a2a0c6cdcda52ccc0
mozaddedalfeshani <mozaddedalfeshanimurad@gmail.com>
2023-12-06 22:26:45 +0600

    Done

---
+++ 1.Structure implementation.cpp
@@ -0,0 +1,10 @@
+class MyStructure
+{
+public:
+    // Constructor
+    MyStructure(int attribute1, int attribute2) : attribute1(attribute1), attribute2(attribute2) {}
+
+    // Public member variables
+    int attribute1;
+    int attribute2;
+};
---
+++ 2.Create a linked list.cpp
@@ -0,0 +1,25 @@
+// Muradian Code
+#include <iostream>
+using namespace std;
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int x) : data(x), next(NULL) {}
+};
+int main()
+{
+    Node *root = new Node(10);
+    root->next = new Node(20);
+    root->next = new Node(30);
+
+    // display all the value
+
+    while (root != nullptr)
+    {
+        cout << root->data << " ";
+        root = root->next;
+    }
+
+    return 0;
+}
